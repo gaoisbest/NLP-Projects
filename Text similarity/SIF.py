49b9a693aa2312190ea09c17364e34bcdb7b75ce
Feng Gao <gaoisbest@users.noreply.github.com>
2018-05-05 19:26:39 +0800

    Update SIF.py

--- Text similarity/SIF.py
+++ Text similarity/SIF.py
@@ -2,7 +2,7 @@
 # Two steps:
 # [1] Compute the weighted average of the word vectors in the sentence.
 # [2] Common component removal: remove the projections of the average vectors on their first principal component.
-# From A simple but tough-to-beat baseline for sentence embeddings, https://openreview.net/pdf?id=SyK00v5xx
+# From paper: A simple but tough-to-beat baseline for sentence embeddings, https://openreview.net/pdf?id=SyK00v5xx
 # Official implementation: https://github.com/PrincetonML/SIF
 
 def SIF_embedding(self, x, w):
@@ -16,15 +16,15 @@ def SIF_embedding(self, x, w):
     emb = np.zeros((n_samples, self.word_vectors.shape[1]))
     for i in range(n_samples):
         emb[i, :] = w[i, :].dot(self.word_vectors[x[i, :], :]) / np.count_nonzero(w[i, :])
+    
     # step 2: removing the projection on the first principal component
     svd = TruncatedSVD(n_components=self.SIF_npc, n_iter=7, random_state=0)
     svd.fit(emb)
     pc = svd.components_
-    if self.SIF_npc == 1:
-        # pc.transpose().shape : embedding_size * 1
-        # emb.dot(pc.transpose()).shape: num_sample * 1
-        # (emb.dot(pc.transpose()) * pc ).shape: num_sample * embedding_size
-        common_component_removal = emb - emb.dot(pc.transpose()) * pc
-    else:
-        common_component_removal = emb - emb.dot(pc.transpose()).dot(pc)
+    
+    # pc.shape: self.SIF_npc * embedding_size
+    # pc.transpose().shape : embedding_size * self.SIF_npc
+    # emb.dot(pc.transpose()).shape: num_sample * self.SIF_npc
+    # (emb.dot(pc.transpose()) * pc ).shape: num_sample * embedding_size
+    common_component_removal = emb - emb.dot(pc.transpose()).dot(pc)
     return common_component_removal
